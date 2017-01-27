7ea44e4116d8f9e25272f63ae309930f7fc58503
John <jebe@bee7.net>
2017-01-27 16:12:34 +0100

    Added Date.timeIntervaleSinceNow as comparison

--- DispatchTimerIssueTest/ViewController.swift
+++ DispatchTimerIssueTest/ViewController.swift
@@ -11,23 +11,30 @@ import UIKit
 class ViewController: UIViewController {
     
     fileprivate var startTime: DispatchTime?
+    fileprivate var startTimeAlternative: Date?
     
     @IBAction func startButtonTapped(_ sender: Any) {
         startTime = DispatchTime.now()
+        startTimeAlternative = Date()
         durationLabel.text = ""
+        alternateResultLabel.text = ""
         
     }
     @IBAction func endButtonTapped(_ sender: Any) {
         
         let endTime = DispatchTime.now()
+        let endTimeAlternative = Date()
+        
         let nanoTime = endTime.uptimeNanoseconds - startTime!.uptimeNanoseconds
         
         let duration = Double(nanoTime) / 1_000_000 // Make it millis
-        
+        let durationAlternative = startTimeAlternative!.timeIntervalSinceNow * -1 * 1000
         durationLabel.text = "\(duration) ms"
+        alternateResultLabel.text = "\(durationAlternative) ms"
     }
     
     @IBOutlet weak var durationLabel: UILabel!
+    @IBOutlet weak var alternateResultLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
